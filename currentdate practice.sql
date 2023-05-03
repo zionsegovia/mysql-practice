edad62c813fd5b052f152c0765ec7eebca444cd1
zion segovia <zionsegovia@gmail.com>
2023-05-03 16:38:19 -0500

    Current date and time practice

---
+++ currentdate practice.sql
@@ -0,0 +1,24 @@
+USE mydb;
+
+
+# create table test
+CREATE TABLE test(
+    my_date DATE,
+    my_time TIME,
+    my_datetime DATETIME
+);
+
+# insert data into the test table (dates)
+INSERT INTO test
+VALUES (CURRENT_DATE(), CURRENT_TIME(),NOW());
+
+# this would make it so you are referring to tomorrow
+# that is essentially what adding the plus one to current date does
+# and it can be adjusted to refer to different dates
+INSERT INTO test
+VALUES(CURRENT_DATE()+1, NULL, NULL);
+
+
+
+SELECT * FROM test;
+
