2090470345cfce00131b469e2cdaa64929cf34d6
zion segovia <zionsegovia@gmail.com>
2023-05-03 00:05:02 -0500

    Practice

---
+++ insertingRows.sql
@@ -0,0 +1,18 @@
+# insert row individually
+
+#INSERT INTO employees
+#VALUES (1,"Eugene","Krabs",25.50,"2023-01-02");
+
+# insert multiple
+
+#INSERT INTO employees
+#VALUES (2, "Squidward", "Tentacles", 12.50, "2023-01-03" ),
+#       (3, "Patrick", "Star", 11, "2023-01-04"),
+#       (4, "Spongebob","Squarepants", 15, "2023-01-03"),
+#       (5, "Sandy","Cheeks",12.25, "2023-01-06");
+
+INSERT INTO employees (employee_id, first_name, last_name)
+VALUES (6,"Sheldon", "Plankton");
+
+SELECT * FROM employees;
+
